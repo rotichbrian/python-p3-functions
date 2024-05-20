446c1e83a89bb9cf6e85ca744e536213aae5742c
Brian Kipkorir <rotichbrian891@gmail.com>
2024-05-20 22:06:50 +0300

    lab completion

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name="Dominique"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Brian")    
 
-def add(num1, num2):
-    pass
+def add(num1=45, num2=55):
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
